4108269158fbac281af535531170e9b26194f931
mrkadriann <nova.markadrian.orsolon@gmail.com>
2025-05-04 14:00:38 +0800

    Initial Commit

--- InventoryManagement/obj/Debug/net9.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement/obj/Debug/net9.0/InventoryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbd343b882bfe5cb51eb8d8ea3569104a9a37e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
