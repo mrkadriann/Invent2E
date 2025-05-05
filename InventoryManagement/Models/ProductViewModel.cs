a0ea50b5ce31d78c93145dde996f73b4ff459951
Arimetsu <vennidictdimayuga@gmail.com>
2025-05-05 19:14:50 +0800

    Add project files and code changes

--- InventoryManagement/Models/ProductViewModel.cs
+++ InventoryManagement/Models/ProductViewModel.cs
@@ -8,5 +8,11 @@
         public string Category { get; set; }
         public decimal? MinPrice { get; set; }
         public decimal? MaxPrice { get; set; }
+        public string StockStatus { get; set; }
+
+        public List<string>  AvailableCategories { get; set; } = new List<string>();
+
+        public List<string> AvailableStockStatuses { get; set; } = new List<string> { "All", "normal", "low", "danger", "overstocked" };
+
     }
 }
