8cf6235e1d86988305bd0b549b9815e866f63bb6
mrkadriann <nova.markadrian.orsolon@gmail.com>
2025-05-16 02:33:15 +0800

    fixed connections excluded appdata

--- InventoryManagement/obj/Debug/net9.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement/obj/Debug/net9.0/InventoryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f68ab58e84847c78f91debf8c2a9673187964519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6139e14aa9244fd8fee7b891ba75728e941dd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
