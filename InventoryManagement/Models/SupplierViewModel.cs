8202a2f70809ae6ec210df29b3ea586f4d1abae2
Arimetsu <vennidictdimayuga@gmail.com>
2025-05-18 15:38:18 +0800

    Updating UI

--- InventoryManagement/Models/SupplierViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace InventoryManagement.Models
-{
-    public class SupplierViewModel
-    {
-    }
-}
--- InventoryManagement/obj/Debug/net9.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement/obj/Debug/net9.0/InventoryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39503ed19b718462eedf1bc4344f56fbf3159af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16861649b42b67fcfd4076213c35575bf1e599d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
